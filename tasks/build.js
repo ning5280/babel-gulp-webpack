a6aa8babdb38ac3a0bffcad3793bda163d00da7f
wangning <529023364@qq.com>
2017-04-26 22:36:50 +0800

    html

--- tasks/build.js
+++ tasks/build.js
@@ -1,4 +1,4 @@
 import gulp from 'gulp';
 import gulpSequence from 'gulp-sequence';
 
-gulp.task('build',gulpSequence('clean','css','scripts',['browser','serve']));
+gulp.task('build',gulpSequence('clean','css','scripts','pages',['browser','serve']));
